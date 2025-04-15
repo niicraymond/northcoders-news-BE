90ec2ad72b34af40fd5c1f8885ca833a46eb55f7
Nicole Raymond <nicoleraymond@hotmail.com>
2025-04-15 16:59:37 +0100

    need to access the article ids

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+## Set Up
+
+1. Clone the repo
+    - git clone https://github.com/niicraymond/northcoders-news-BE.git 
+
+2. Install dependencies 
+    - npm install
+
+3. Create the environment files:
+
+- .env.development
+    - PGDATABASE=nc_news
+
+- .env.test
+    - PGDATABASE=nc_news_test
+
+4. Set up the databases:
+    - npm run setup-dbs
+
+5. Seed the development database:
+    - NODE_ENV=development npm run seed-dev
+
+6. Run tests:
+    - npm test
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,116 @@
-const db = require("../connection")
-
+const db = require("../connection");
+const format = require("pg-format");
+const { convertTimestampToDate } = require("./utils");
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments, articles, users, topics`)
+    .then(() => {
+      return db.query(
+        `CREATE TABLE topics(
+          slug VARCHAR PRIMARY KEY,
+          description TEXT NOT NULL,
+          img_url TEXT
+        );`
+      );
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users(
+          username VARCHAR UNIQUE PRIMARY KEY,
+          name VARCHAR NOT NULL,
+          avatar_url TEXT
+        );`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE articles(
+          article_id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          topic VARCHAR NOT NULL REFERENCES topics(slug),
+          author VARCHAR NOT NULL REFERENCES users(username),
+          body TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT NOW(),
+          votes INT DEFAULT 0,
+          article_img_url TEXT
+        );`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE comments(
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT NOT NULL REFERENCES articles(article_id), 
+          body TEXT NOT NULL,
+          votes INT DEFAULT 0,
+          author VARCHAR NOT NULL REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT NOW()
+        );`);
+      // ^^ article_id should be not null? Why does comments data refer to article_title?
+    })
+    .then(() => {
+      const formattedTopics = topicData.map((topic) => {
+        return [topic.slug, topic.description, topic.img_url];
+      });
+
+      const insertTopics = format(
+        `INSERT INTO topics (slug, description, img_url) VALUES %L`,
+        formattedTopics
+      );
+
+      return db.query(insertTopics);
+    })
+    .then(() => {
+      const formattedUsers = userData.map((user) => {
+        return [user.username, user.name, user.avatar_url];
+      });
+
+      const insertUsers = format(
+        `INSERT INTO users (username, name, avatar_url) VALUES %L`,
+        formattedUsers
+      );
+
+      return db.query(insertUsers);
+    })
+    .then(() => {
+      const formattedArticles = articleData.map((article) => {
+        const converted = convertTimestampToDate(article);
+        return [
+          converted.title,
+          converted.topic,
+          converted.author,
+          converted.body,
+          converted.created_at,
+          converted.votes,
+          converted.article_img_url,
+        ];
+      });
+
+      const insertArticles = format(
+        `INSERT INTO articles (title, topic, author, body, created_at, votes, article_img_url) VALUES %L RETURNING *`,
+        formattedArticles
+      );
+
+      return db.query(insertArticles);
+    })
+    .then((articles) => {
+      console.log(articles.rows);
+      const formattedComments = commentData.map((comment) => {
+        const converted = convertTimestampToDate(comment);
+        // console.log(converted);
+        return [
+          converted.body,
+          converted.votes,
+          converted.author,
+          converted.article_id,
+          converted.created_at,
+        ];
+      });
+
+      const insertComments = format(
+        `INSERT INTO comments (body, votes, author, article_id, created_at) VALUES %L`,
+        formattedComments
+      );
+
+      return db.query(insertComments);
+    });
 };
 module.exports = seed;
