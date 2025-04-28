c20a6fca463d6430d344d0ab6a4e55778483f0a9
Nicole Raymond <nicoleraymond@hotmail.com>
2025-04-28 16:54:10 +0100

    adds get articles by id endpoint

--- app/models/model.js
+++ app/models/model.js
@@ -1,8 +1,19 @@
-const db = require('../../db/connection')
+const db = require("../../db/connection");
 
 exports.selectTopics = () => {
+  return db.query(`SELECT slug, description FROM topics;`).then((result) => {
+    return result.rows;
+  });
+};
 
-    return db.query(`SELECT slug, description FROM topics;`).then((result) => {
-        return result.rows
-    })
-}
+exports.selectArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      }
+
+      return result.rows[0];
+    });
+};
