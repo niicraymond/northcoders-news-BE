dfd2a1b8c0e198a4f7425ddebb9d69ec50e29d04
Nicole Raymond <nicoleraymond@hotmail.com>
2025-04-28 16:02:44 +0100

    completes endpoint but need to fix 404 test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const request = require('supertest')
 const app = require('../app.js')
 const db = require('../db/connection.js')
 const seed = require('../db/seeds/seed.js')
-const data = require('../db/data/test-data')
+const data = require('../db/data/test-data/index.js')
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
@@ -26,3 +26,29 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects with a slug and description property", () => {
+    return request(app)
+    .get("/api/topics")
+    .expect(200)
+    .then((response) => {
+      const topics = response.body.topics
+      expect(Array.isArray(topics)).toBe(true)
+      topics.forEach((topic) => {
+        expect(topic).toMatchObject({
+          slug: expect.any(String),
+          description: expect.any(String)
+        })
+      })
+    })
+  })
+  test.skip("404: Responds a 404 error if given an incorrect path", () => {
+    return request(app)
+    .get("/api/incorrectpath")
+    .expect(404)
+    .then((response) => {
+      expect(response.body.msg).toBe("Path not found")
+    })
+  })
+})
